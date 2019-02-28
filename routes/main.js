982ae5fd1d4f2158403597f9428007463bf01f4e
gglee89 <gglee89@gmail.com>
2019-02-28 14:57:37 -0600

    Separate HTML from response into a separate variable

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,15 @@
-module.exports = app => {
-  app.get('/', function(request, response) {
-    response.send(`
-      <!DOCTYPE html>
-      <html>
-      <body>
-
+const html = `
+  <!DOCTYPE html>
+  <html>
+    <body>
       <h1>My First Heading</h1>
       <p>My first paragraph.</p>
+    </body>
+  </html>
+`;
 
-      </body>
-      </html>
-
-          `);
+module.exports = app => {
+  app.get('/', function(request, response) {
+    response.send(html);
   });
 };
